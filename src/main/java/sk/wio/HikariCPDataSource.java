87002d105d5428c686ee74754051de48402abd1e
Wio44 <will@zoznam.sk>
2024-11-11 15:50:34 +0100

    Load db connection values from properties

--- src/main/java/sk/wio/HikariCPDataSource.java
+++ src/main/java/sk/wio/HikariCPDataSource.java
@@ -2,20 +2,31 @@ package sk.wio;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class HikariCPDataSource {
     private static HikariConfig config = new HikariConfig();
     private static final HikariDataSource ds;
+    private static Logger logger = LoggerFactory.getLogger(HikariCPDataSource.class);
 
     static {
-        config.setJdbcUrl("jdbc:mysql://localhost:3306/contacts");
-        config.setUsername("root");
-        config.setPassword("password");
+        final Properties prop = new Properties();
+        try {
+            prop.load(HikariCPDataSource.class.getResourceAsStream("/application.properties"));
+        } catch (Exception e) {
+            logger.error("Error while loading application.properties file!", e);
+        }
 
-        ds = new  HikariDataSource(config);
+        config.setJdbcUrl("jdbc:mysql://localhost:3306/" + prop.getProperty("db.name"));
+        config.setUsername(prop.getProperty("db.username"));
+        config.setPassword(prop.getProperty("db.password"));
+
+        ds = new HikariDataSource(config);
     }
 
     public static Connection getConnection() throws SQLException {
