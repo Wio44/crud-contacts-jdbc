30e3d2c028c02a4ddc5714e0d55bfb134229e298
Wio44 <will@zoznam.sk>
2024-11-11 12:48:11 +0100

    Connect to MySQL database and print contacts in Main

--- src/main/java/sk/wio/Main.java
+++ src/main/java/sk/wio/Main.java
@@ -1,7 +1,30 @@
 package sk.wio;
 
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
+        String connectionUrl = "jdbc:mysql://localhost:3306/contacts";
+        String select = "SELECT * FROM contact";
 
+        try {
+            Connection connection = DriverManager.getConnection(connectionUrl, "root", "password");
+            PreparedStatement statement = connection.prepareStatement(select);
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                String email = resultSet.getString("email");
+                String phone = resultSet.getString("phone");
+
+                System.out.println("id: " + id + ", name: " + name + ", email: " + email + ", phone: " + phone);
+
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Error while connecting to database");
+        }
     }
 }
